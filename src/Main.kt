73de806fdee7fa2375c5eecce8ae686f9deca68b
GUSTAVO VIDAL DE ABREU <25203686@dsn127m1246733.cfp127aluno.local>
2026-08-17 16:56:08 -0300

    feat: enviar dados adicionados

--- src/Main.kt
+++ src/Main.kt
@@ -5,17 +5,21 @@
  **************************************************/
 
 fun main() {
-    // 1. Saída de dados
+    // 1. SAÍDA DE DADOS
     println("Hello World!")
 
-    // 2. Criação de variáveis
+
+
+
+
+    // 2. CRIAÇÃO DE VARIÁVEIS
     val pi = 3.14
     var idade = 27
     println(idade)
     idade = 28
     println(idade)
 
-    // 3. Criação de variáveis tipadas
+    // 2.1. Criação de variáveis tipadas
     var idadeTipada: Int = 27
     idadeTipada = 28
     println(idadeTipada)
@@ -23,19 +27,19 @@ fun main() {
     alturaTipada = 2.0
     println(alturaTipada)
 
-    // 4. Criação de variáveis String
+    // 2.2. Criação de variáveis String
     val texto: String = "Bom dia, pessoal!"
     println(texto)
     println(texto.length)
     println(texto.uppercase())
     println(texto.contains("pessoal"))
 
-    // 5. Criação de variáveis interpoladas
+    // 2.3. Criação de variáveis interpoladas
     val nomeInterpolado: String = "vidal"
     val idadeInterpolada = 18
     println("Bom dia $nomeInterpolado! Logo logo você fará ${idadeInterpolada + 1} anos")
 
-    // 6. Criação de variáveis String quebradas
+    // 2.4. Criação de variáveis String quebradas
     val textoQuebrado = """
         Texto escrito
         em várias linhas
@@ -44,20 +48,44 @@ fun main() {
     println(textoQuebrado)
     println(textoQuebrado.trimIndent())
 
-    // 7. Criação de variáveis Char
+
+
+
+
+    // 3. TIPOS DE DADOS
+    // 3.1. Criação de variáveis Char
     val caracter = "$"
     val letra: Char = 'j'
     println(letra)
 
-    // 8. Criação de variáveis Boolean
+    // 3.2. Criação de variáveis Boolean
     val bloqueado: Boolean = false
     println(bloqueado)
 
-    // 9. Criação de variáveis Int
+    // 3.3. Criação de variáveis Int
     val idadeInt: Int = 27
     println(idadeInt)
 
-    // 10. Criação de variáveis Long
-    val populacaoLong: Long = 7000000000L
+    // 3.4. Criação de variáveis Long
+    val populacaoLong: Long = 7_000_000_000L
     println(populacaoLong)
+
+    // 3.5. Criação de variáveis Float
+    val pesoFloat: Float = 55.2350943543983753065875485487638754F
+    println(pesoFloat)
+
+    // 3.6. Criação de variáveis Double
+    val alturaDouble: Double = 1.802409050724592484589274975267887548
+    println(alturaDouble)
+
+
+
+
+
+    // 4. CONVERSÕES
+    val x: Int = 5
+    val y: Double = x.toDouble() // obrigatório
+    // val z: Double = x // não funciona
+    println(x)
+    println(y)
 }
